8c0edd7706a48ebb9d49607b56e74bde0f5a464a
olebolesparabol <mortenk@hotmail.com>
2022-02-27 12:56:23 +0100

    Refactored buildManager to use object variables

--- buildManager.js
+++ buildManager.js
@@ -1,53 +1,56 @@
 var buildManager = {
+    currentRoom: {},
+    extensionPositions: {},
     run: function(room, spawner) {
+        this.currentRoom = room;
         //if(!Memory.planning.extensionsPlanned) {
             console.log("Executing extension planning");
-            let extensionPositions = this.planExtensions(room, spawner);
+            this.extensionPositions = this.planExtensions(spawner);
             
             switch(room.controller.level) {
                 case 2:
-                    this.buildExtensions(room, extensionPositions, 5);
+                    this.buildExtensions(5);
                     break;
                 case 3:
-                    this.buildExtensions(room, extensionPositions, 10);
+                    this.buildExtensions(10);
                     break;
                 case 4:
-                    this.buildExtensions(room, extensionPositions, 12);
+                    this.buildExtensions(12);
                     break;
                 default:
                     break;
             }
         //}
     },
-    planExtensions: function(room, spawner) {
+    planExtensions: function(spawner) {
         let sx = spawner.pos.x;
         let sy = spawner.pos.y;
         
         let extensionMatrix = new Array(12);
-        extensionMatrix[0] = new RoomPosition(sx-2,sy-2, room.name);
-        extensionMatrix[1] = new RoomPosition(sx-1,sy-2, room.name);
-        extensionMatrix[2] = new RoomPosition(sx-2,sy-1, room.name);
+        extensionMatrix[0] = new RoomPosition(sx-2,sy-2, this.currentRoom.name);
+        extensionMatrix[1] = new RoomPosition(sx-1,sy-2, this.currentRoom.name);
+        extensionMatrix[2] = new RoomPosition(sx-2,sy-1, this.currentRoom.name);
         
-        extensionMatrix[3] = new RoomPosition(sx+2,sy-2, room.name);
-        extensionMatrix[4] = new RoomPosition(sx+1,sy-2, room.name);
-        extensionMatrix[5] = new RoomPosition(sx+2,sy-1, room.name);
+        extensionMatrix[3] = new RoomPosition(sx+2,sy-2, this.currentRoom.name);
+        extensionMatrix[4] = new RoomPosition(sx+1,sy-2, this.currentRoom.name);
+        extensionMatrix[5] = new RoomPosition(sx+2,sy-1, this.currentRoom.name);
         
-        extensionMatrix[6] = new RoomPosition(sx-2,sy+2, room.name);
-        extensionMatrix[7] = new RoomPosition(sx-2,sy+1, room.name);
-        extensionMatrix[8] = new RoomPosition(sx-1,sy+2, room.name);
+        extensionMatrix[6] = new RoomPosition(sx-2,sy+2, this.currentRoom.name);
+        extensionMatrix[7] = new RoomPosition(sx-2,sy+1, this.currentRoom.name);
+        extensionMatrix[8] = new RoomPosition(sx-1,sy+2, this.currentRoom.name);
         
-        extensionMatrix[9] = new RoomPosition(sx+2,sy+2, room.name);
-        extensionMatrix[10] = new RoomPosition(sx+2,sy+1, room.name);
-        extensionMatrix[11] = new RoomPosition(sx+1,sy+2, room.name);
+        extensionMatrix[9] = new RoomPosition(sx+2,sy+2, this.currentRoom.name);
+        extensionMatrix[10] = new RoomPosition(sx+2,sy+1, this.currentRoom.name);
+        extensionMatrix[11] = new RoomPosition(sx+1,sy+2, this.currentRoom.name);
         
         return extensionMatrix;
     },
-    buildExtensions: function(room, extensionPositions, desiredExtensionCount) {
-        let existingExtensions = room.find(FIND_MY_STRUCTURES, {
+    buildExtensions: function(desiredExtensionCount) {
+        let existingExtensions = this.currentRoom.find(FIND_MY_STRUCTURES, {
             filter: { structureType: STRUCTURE_EXTENSION }
         });
 
-        let plannedExtensions = room.find(FIND_MY_CONSTRUCTION_SITES, {
+        let plannedExtensions = this.currentRoom.find(FIND_MY_CONSTRUCTION_SITES, {
             filter: { structureType: STRUCTURE_EXTENSION }
         });
         console.log("Existing extensions: " + existingExtensions.length);
@@ -57,7 +60,7 @@ var buildManager = {
         let i = 0;
         while (totalExtensions < desiredExtensionCount) {
             console.log("Planning: " + i)
-            let result = room.createConstructionSite(extensionPositions[i], STRUCTURE_EXTENSION);
+            let result = this.currentRoom.createConstructionSite(this.extensionPositions[i], STRUCTURE_EXTENSION);
             if (result == OK) {
                 totalExtensions++;
             }
